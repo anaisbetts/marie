97537689ecef34c9dd50eba7a94826897c795a17
Anaïs Betts <anais@anaisbetts.org>
2020-12-05 16:39:35 +0100

    Add censored IDs

---
+++ components/firebase-config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
+};
