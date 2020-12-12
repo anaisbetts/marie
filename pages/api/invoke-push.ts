461879d1133a88ac6f5fea8c271a5c1dbc47d3f4
Anaïs Betts <anais@anaisbetts.org>
2020-12-12 20:38:32 +0100

    Correctly configure Firebase Admin SDK

--- pages/api/invoke-push.ts
+++ pages/api/invoke-push.ts
@@ -18,18 +18,8 @@ const client = new Draqula(process.env.NEXT_PUBLIC_DB_URL, {
   },
 });
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
-};
-
-firebase.initializeApp(firebaseConfig);
+// NB: This uses the FIREBASE_CONFIG env var
+firebase.initializeApp();
 
 interface PushRow {
   created_at: Date;
