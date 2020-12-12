c36f0447141923f0081c5e4b40d5e74f215747ff
Anaïs Betts <anais@anaisbetts.org>
2020-12-12 22:17:12 +0100

    loose-envify doesn't work on minified files, change our approach

--- script/fixup-sw.sh
+++ script/fixup-sw.sh
@@ -5,11 +5,13 @@ set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT="$DIR/.."
 
-files=$(ls $ROOT/public/worker-*.js)
+pushd .
+cd $ROOT/worker-env
+files=$(ls *.js)
+popd
 
+mkdir -p "$ROOT/worker"
 for f in $files
 do
-  $ROOT/node_modules/.bin/loose-envify "$f" > /tmp/envjs
-  rm "$f"
-  mv /tmp/envjs "$f"
+  $ROOT/node_modules/.bin/loose-envify "$ROOT/worker-env/$f" > "$ROOT/worker/$f"
 done
