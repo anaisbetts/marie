0b8566ce5d517e3d8916a417ecaf8a748038a114
Anaïs Betts <anais@anaisbetts.org>
2020-12-13 22:23:32 +0100

    Start to define a color scheme

--- components/theme.ts
+++ components/theme.ts
@@ -2,15 +2,21 @@ import type { TypographyOptions } from 'typography';
 import Typography from 'typography';
 import themeOptions from 'typography-theme-moraga';
 
-export const typography = new Typography({
+const theme: TypographyOptions = {
   ...themeOptions,
   headerFontFamily: ['Martel Sans', 'sans-serif'],
-  headerWeight: 400,
+  headerWeight: 200,
   googleFonts: [
     ...themeOptions.googleFonts,
     {
       name: 'Martel Sans',
-      styles: ['400'],
+      styles: ['200'],
     },
   ],
-});
+};
+
+// Let's handle this in CSS instead
+delete theme.bodyColor;
+delete theme.headerColor;
+
+export const typography = new Typography(theme);
