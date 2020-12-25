856915ebf2093a3e8d3999afd27a60ed432de7c0
Anaïs Betts <anais@anaisbetts.org>
2020-12-25 22:27:32 +0100

    Create a page that embeds the current time via SSR to see what happens

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,8 +1,20 @@
 import * as React from 'react';
+
+import { GetServerSideProps } from 'next';
 import { Scaffold } from '../components/scaffold';
 
-const SettingsPage: React.FC = () => {
-  return <Scaffold buttonIndex={1} title="Settings" />;
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    props: { now: new Date().toISOString() },
+  };
+};
+
+const SettingsPage: React.FC<{ now: string }> = ({ now }) => {
+  return (
+    <Scaffold buttonIndex={1} title="Settings">
+      <h3>{`The page was generated on ${now}`}</h3>
+    </Scaffold>
+  );
 };
 
 export default SettingsPage;
