afb39b3ac5062a617f2b4cae1706764b5862a12c
Anaïs Betts <anais@anaisbetts.org>
2021-01-03 20:29:09 +0100

    First hack at image upload

--- pages/new-mess.tsx
+++ pages/new-mess.tsx
@@ -1,13 +1,22 @@
+import * as React from 'react';
+import { useState } from 'react';
+import Image from 'next/image';
+
 import { faCamera } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import * as React from 'react';
+
 import { Scaffold } from '../components/scaffold';
-import {
-  AuthedUploadButton,
-  SignedUploadButton,
-} from '../components/signed-upload-button';
+import { AuthedUploadButton } from '../components/signed-upload-button';
 
 const SettingsPage: React.FC = () => {
+  const [imageUrl, setImageUrl] = useState<string>(null);
+
+  const imageContent = imageUrl ? (
+    <img src={imageUrl} style={{ maxHeight: 300 }} />
+  ) : (
+    <FontAwesomeIcon icon={faCamera} size="10x" />
+  );
+
   return (
     <Scaffold buttonIndex={0} title="New Mess" showNav={false} showBack>
       <style jsx>{`
@@ -35,11 +44,38 @@ const SettingsPage: React.FC = () => {
           margin-right: 32px;
           margin-top: 16px;
         }
+
+        .camera {
+          height: 300px;
+          width: 100%;
+
+          margin-top: 16px;
+          margin-bottom: 16px;
+
+          color: hsla(var(--text-hs), var(--text-l), 0.2);
+
+          display: grid;
+          align-content: center;
+          justify-content: center;
+          grid-column: 1fr;
+          position: relative;
+        }
+
+        .camera img {
+          max-height: 300px;
+          max-width: 100%;
+        }
       `}</style>
+
       <section>
         <h3>Take a photo of this mess!</h3>
-        <p>[the mess]</p>
-        <AuthedUploadButton />
+        <div className="camera">{imageContent}</div>
+        <AuthedUploadButton onSubmitted={(e) => setImageUrl(e.imageUrl)}>
+          <>
+            <FontAwesomeIcon icon={faCamera} size="lg" />
+            &nbsp;&nbsp;Take a Photo
+          </>
+        </AuthedUploadButton>
 
         <h4>Who would know best where these things go?</h4>
         <select>
